09e2e81d0aafae42ff444cb381b8625b66b517bd
Douglas Cardoso <douglas.dcm@gmail.com>
2019-02-25 21:15:28 -0300

    fisrt

---
+++ CursoSelenium/src/main/java/TesteGoogle.java
@@ -0,0 +1,33 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+
+
+
+public class TesteGoogle {
+	
+	@Test
+	public void test() throws InterruptedException {
+		//System.setProperty("webdriver.gecko.driver", "C:\\Users\\Douglas\\Google Drive\\Pessoais\\Estudos\\Selenium\\geckodriver-v0.23.0-win64\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		//WebDriver driver = new ChromeDriver();
+		//WebDriver driver = new InternetExplorerDriver();
+		//String parentWindowHandle = driver.getWindowHandle();
+		//driver.manage().window().setPosition(new Point(100, 100));
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		//driver.manage().window().maximize();
+		driver.get("http://www.google.com");
+		//driver.switchTo().window(parentWindowHandle);
+		Thread.sleep(3000);
+		Assert.assertEquals("Google", driver.getTitle());
+		driver.quit();
+	}
+
+}
